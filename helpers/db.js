d7440088d5ecfedcc74857c446ea9805c93ad0ae
Mustafa Bay <msty.bay@gmail.com>
2024-08-22 21:50:32 +0300

    Update db.js

--- helpers/db.js
+++ helpers/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://msty:1910musty@cluster0.60j8ntu.mongodb.net/Musty");
+        await mongoose.connect("");
         console.log(`MongoDB connected`);
     }
     catch (err) {
